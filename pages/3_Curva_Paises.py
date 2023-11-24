d39758583f6c4ee3ab506c7a001bf31c0f2d8b3d
Alessandro Coronado Siles <alecoronadosiles@hotmail.com>
2023-11-24 19:18:20 +0000

    sfe3

--- pages/3_Curva_Paises.py
+++ pages/3_Curva_Paises.py
@@ -13,25 +13,34 @@ def process_dataframe(xls_path):
         desembolsos = xls.parse('Desembolsos')
         operaciones = xls.parse('Operaciones')
 
-    # Asegúrate de que las columnas 'SECTOR' y 'SUBSECTOR' estén en 'operaciones'
+    # Verificar que las columnas 'SECTOR' y 'SUBSECTOR' estén en 'operaciones'
+    if 'SECTOR' not in operaciones.columns or 'SUBSECTOR' not in operaciones.columns:
+        raise ValueError("Las columnas SECTOR y SUBSECTOR son necesarias en la hoja 'Operaciones'")
+
+    # Mezclar los DataFrames
     merged_df = pd.merge(desembolsos, operaciones[['IDEtapa', 'FechaVigencia', 'AporteFonplata', 'SECTOR', 'SUBSECTOR']], on='IDEtapa', how='left')
+
+    # Convertir fechas a datetime y calcular año y meses
     merged_df['FechaEfectiva'] = pd.to_datetime(merged_df['FechaEfectiva'], dayfirst=True)
     merged_df['FechaVigencia'] = pd.to_datetime(merged_df['FechaVigencia'], dayfirst=True)
     merged_df['Ano'] = ((merged_df['FechaEfectiva'] - merged_df['FechaVigencia']).dt.days / 366).astype(int)
     merged_df['Meses'] = ((merged_df['FechaEfectiva'] - merged_df['FechaVigencia']).dt.days / 30).astype(int)
 
+    # Agrupar y calcular montos acumulados y porcentajes
     result_df = merged_df.groupby(['IDEtapa', 'Ano', 'Meses', 'IDDesembolso', 'AporteFonplata'])['Monto'].sum().reset_index()
-    result_df['Monto Acumulado'] = result_df.groupby(['IDEtapa'])['Monto'].cumsum().reset_index(drop=True)
+    result_df['Monto Acumulado'] = result_df.groupby(['IDEtapa'])['Monto'].cumsum()
     result_df['Porcentaje del Monto'] = result_df['Monto'] / result_df['AporteFonplata'] * 100
     result_df['Porcentaje del Monto Acumulado'] = result_df['Monto Acumulado'] / result_df['AporteFonplata'] * 100
 
+    # Mapear códigos de país a nombres
     country_map = {'AR': 'Argentina', 'BO': 'Bolivia', 'BR': 'Brasil', 'PY': 'Paraguay', 'UR': 'Uruguay'}
     result_df['Pais'] = result_df['IDEtapa'].str[:2].map(country_map).fillna('Desconocido')
 
-    # Añadir 'SECTOR', 'SUBSECTOR' y 'FechaVigencia' 'APODO' al DataFrame resultante
-    result_df = pd.merge(result_df, operaciones[['IDEtapa', 'SECTOR', 'SUBSECTOR', 'FechaVigencia','APODO']],desembolsos['FechaEfectiva'], on='IDEtapa', how='left')
+    # Añadir 'SECTOR', 'SUBSECTOR', 'FechaVigencia' y 'APODO' al DataFrame resultante
+    result_df = pd.merge(result_df, operaciones[['IDEtapa', 'SECTOR', 'SUBSECTOR', 'FechaVigencia', 'APODO']], on='IDEtapa', how='left')
 
     return result_df
+
 
 def run():
     st.set_page_config(
